f98e229831345e5c12e4c473d793ff6bec24d21e
victor-estrada <victorestrada.ux@gmail.com>
2017-05-04 23:02:31 -0500

    updated server file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
 app.use(bodyParser.json());
-
+  
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
